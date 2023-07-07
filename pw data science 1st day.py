8891866b64ef67e863cd49d9f6120f6ae53a9ea0
manufccr007 <107296627+manufccr007@users.noreply.github.com>
2023-07-07 07:42:05 +0300

    Add files via upload

---
+++ pw data science 1st day.py
@@ -0,0 +1,1107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+a=10
+
+
+# In[2]:
+
+
+b=20
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+a+b
+
+
+# In[4]:
+
+
+type(a+b)
+
+
+# In[5]:
+
+
+b=3.75
+
+
+# In[6]:
+
+
+type(b)
+
+
+# In[7]:
+
+
+a=("sk")
+
+
+# In[8]:
+
+
+a
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+type(a)
+
+
+# In[10]:
+
+
+d=True
+
+
+# In[11]:
+
+
+type(d)
+
+
+# In[12]:
+
+
+True-False
+
+
+# In[13]:
+
+
+False-False
+
+
+# In[14]:
+
+
+a=False-True
+
+
+# In[15]:
+
+
+a
+
+
+# In[16]:
+
+
+type(a)
+
+
+# In[17]:
+
+
+True/True
+
+
+# In[18]:
+
+
+False/False
+
+
+# In[ ]:
+
+
+#this is for complex data type.
+hi there
+e=4+8J
+
+
+# In[ ]:
+
+
+e
+
+
+# In[ ]:
+
+
+e.real
+
+
+# In[ ]:
+
+
+e.imag
+
+
+# In[ ]:
+
+
+s="sudh"
+
+
+# In[ ]:
+
+
+b=(str(10)+s)
+
+
+# In[ ]:
+
+
+b
+
+
+# In[ ]:
+
+
+b=input()
+
+
+# In[ ]:
+
+
+type(b)
+
+
+# In[ ]:
+
+
+b=int(input())
+
+
+# In[ ]:
+
+
+type(b)
+
+
+# In[ ]:
+
+
+S="Sudh"
+
+
+# In[ ]:
+
+
+s
+
+
+# In[ ]:
+
+
+s
+
+
+# In[ ]:
+
+
+s=10
+
+
+# In[ ]:
+
+
+s
+
+
+# In[ ]:
+
+
+b=10+int(s)
+
+
+# In[ ]:
+
+
+b
+
+
+# In[ ]:
+
+
+L=[1,2,3,"SUDH",True, 4+7J]
+
+
+# In[ ]:
+
+
+L
+
+
+# In[ ]:
+
+
+L[3]
+
+
+# In[ ]:
+
+
+L[-1]
+
+
+# In[ ]:
+
+
+L[3]=20
+
+
+# In[ ]:
+
+
+L
+
+
+# In[ ]:
+
+
+M="SUDH"
+
+
+# In[ ]:
+
+
+M[3]
+
+
+# In[ ]:
+
+
+M[3]=5
+
+
+# In[ ]:
+
+
+1>2
+
+
+# In[ ]:
+
+
+1<3
+
+
+# In[ ]:
+
+
+2!=3
+
+
+# In[ ]:
+
+
+2>=2
+
+
+# In[ ]:
+
+
+2<=3
+
+
+# In[ ]:
+
+
+2>=2
+
+
+# In[ ]:
+
+
+1=2
+
+
+# In[ ]:
+
+
+1==2
+
+
+# In[ ]:
+
+
+True * True
+
+
+# In[ ]:
+
+
+True * False
+
+
+# In[ ]:
+
+
+False * False
+
+
+# In[ ]:
+
+
+True/True
+
+
+# In[ ]:
+
+
+True and False
+
+
+# In[ ]:
+
+
+True or False
+
+
+# In[ ]:
+
+
+10 & 3
+
+
+# In[ ]:
+
+
+bin(10)
+
+
+# In[ ]:
+
+
+23 & 3
+
+
+# In[ ]:
+
+
+bin(23)
+
+
+# In[ ]:
+
+
+bin(3)
+
+
+# In[ ]:
+
+
+34 | 23
+
+
+# In[ ]:
+
+
+bin(34)
+
+
+# In[ ]:
+
+
+bin(23)
+
+
+# In[ ]:
+
+
+bin(55)
+
+
+# In[ ]:
+
+
+a=10
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+a:
+
+
+# In[ ]:
+
+
+a=34
+
+
+# In[ ]:
+
+
+type(a)
+
+
+# In[ ]:
+
+
+a=32.5
+
+
+# In[ ]:
+
+
+type(a)
+
+
+# In[ ]:
+
+
+a="sudh"
+
+
+# In[ ]:
+
+
+a
+
+
+# In[ ]:
+
+
+list=[34,36,37,38,"iftikar", "rock"]
+
+
+# In[ ]:
+
+
+list
+
+
+# In[ ]:
+
+
+list[1]
+
+
+# In[ ]:
+
+
+list[4]=-20
+
+
+# In[ ]:
+
+
+list
+
+
+# In[ ]:
+
+
+S = 'SUDH'
+
+
+# In[ ]:
+
+
+S
+
+
+# In[ ]:
+
+
+False * False 
+
+
+# In[ ]:
+
+
+True/True
+
+
+# In[ ]:
+
+
+False/False
+
+
+# In[ ]:
+
+
+e=4i+6J
+
+
+# In[ ]:
+
+
+e=4+6J
+
+
+# In[ ]:
+
+
+e
+
+
+# In[ ]:
+
+
+e.real
+
+
+# In[ ]:
+
+
+e.imag
+
+
+# In[ ]:
+
+
+e=4+6j
+
+
+# In[ ]:
+
+
+e.real
+
+
+# In[ ]:
+
+
+e.imag
+
+
+# In[ ]:
+
+
+S=('SUDH')
+
+
+# In[ ]:
+
+
+T=S+str(10)
+
+
+# In[ ]:
+
+
+T
+
+
+# In[ ]:
+
+
+b=input()
+
+
+# In[ ]:
+
+
+b
+
+
+# In[ ]:
+
+
+c=1+4j
+
+
+# In[ ]:
+
+
+b=int(input())
+
+
+# In[ ]:
+
+
+b
+
+
+# In[ ]:
+
+
+type(b)
+
+
+# In[ ]:
+
+
+10+10
+
+
+# In[ ]:
+
+
+1>2
+
+
+# In[ ]:
+
+
+2>1
+
+
+# In[ ]:
+
+
+2/2
+
+
+# In[ ]:
+
+
+10&10
+
+
+# In[ ]:
+
+
+bin(10)
+
+
+# In[ ]:
+
+
+53 & 54
+
+
+# In[ ]:
+
+
+bin(53)
+
+
+# In[ ]:
+
+
+bin(54)
+
+
+# In[ ]:
+
+
+10|10
+
+
+# In[ ]:
+
+
+bin(10)
+
+
+# In[ ]:
+
+
+23|25
+
+
+# In[ ]:
+
+
+bin(23)
+
+
+# In[ ]:
+
+
+bin(25)
+
+
+# In[ ]:
+
+
+bin(31)
+
+
+# In[ ]:
+
+
+30>>2
+
+
+# In[ ]:
+
+
+bin(30)
+
+
+# In[ ]:
+
+
+bin(7)
+
+
+# In[ ]:
+
+
+24>>3
+
+
+# In[ ]:
+
+
+bin(24)
+
+
+# In[ ]:
+
+
+bin(3)
+
+
+# In[ ]:
+
+
+24<<3
+
+
+# In[ ]:
+
+
+bin(24)
+
+
+# In[ ]:
+
+
+bin(192)
+
+
+# In[ ]:
+
+
+5/5
+
+
+# In[ ]:
+
+
+5%
+
+
+# In[ ]:
+
+
+5//5
+
+
+# In[ ]:
+
+
+20//5
+
+
+# In[ ]:
+
+
+5**2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+4//3
+
+
+# In[ ]:
+
+
+20//5
+
+
+# In[ ]:
+
+
+22//5
+
+
+# In[ ]:
+
+
+4%3
+
+
+# In[ ]:
+
+
+20%5
+
+
+# In[ ]:
+
+
+10%10
+
+
+# In[ ]:
+
+
+15 % 5
+
+
+# In[ ]:
+
+
+12 % 5
+
+
+# In[ ]:
+
+
+22 % 4
+
+
+# In[ ]:
+
+
+Var='1,2,3'
+
+
+# In[ ]:
+
+
+type(Var)
+
+
+# In[ ]:
+
+
+Var2='[DS, ML, Python]'
+
+
+# In[ ]:
+
+
+type(Var2)
+
+
+# In[ ]:
+
+
+Var3 = ['DS','ML','PYTHON']
+
+
+# In[ ]:
+
+
+type(Var3)
+
+
+# In[ ]:
+
+
+Var4=1
+
+
+# In[ ]:
+
+
+type(Var4)
+
+
+# In[ ]:
+
+
+a=10
+
+
+# In[ ]:
+
+
+a=int(input())
+if a>-10:
+    print('True')
+elif a <15:
+    print('ok')
+else:
+    print('False')
+
+
+# In[ ]:
+
+
+n=10
+i=1
+while i<n:
+    print(i)
+    i=i+1
+    if i == 3
+    break
+else:
+    print("ska jksdnska ")
+
+
+# In[ ]:
+
+
+i
+
+
+# In[ ]:
+
+
+n=20
+i=1
+while i<n:
+    print(i)
+    i=i+1
+    if i==25:
+        break
+else:
+    print('no')
+
+
+# In[ ]:
+
+
+n=25
+i=2
+while i>20:
+    print(i)
+    i=n+2
+else:
+    print('no')
+
+
+# In[ ]:
+
+
+n=10
+i=1
+while i>n:
+    print(i)
+    i=i+1
+else:
+    print('')
+
+
+# In[ ]:
+
+
+i
+
+
+# ####break time
+
+# #break
+
+# In[ ]:
+
+
+"""
+the theh eh
+hehe 
+ehhe
+"""
+a=20
+a
+
+
+# In[ ]:
+
+
+name="sk iftikar"
+age=29
+degree= "BE"
+print('My age is {} and i have completed:my degree of {} with the name of {}'.format(age,degree,name))
+
+
+# In[ ]:
+
+
+age=int(input("Enter your age: "))
+if age > 18 and age<=45 :
+    print("You are young blood")
+else:
+    print("sorry you are not eligible for this")
+
+
+# In[ ]:
+
+
+product=int(input("Enter the Price of the product:"))
+if product >=1000:
+    print(" Congratulations! you are eligible for 20% discount offer")
+else:
+    print("sorry, you are not eligible for discount")
+
+
+# In[ ]:
+
+
+product=int(input('product_price: '))
+if product >= 1000:
+     print('congratulation you have received 20% discount: price after discount{}'.format(product*0.8))
+else:
+    print(' you have received 5% discount: price after discount{}'.format(product*0.05))
+    
+
+
+# In[ ]:
+
+
+product_price= int(input('enter the product price: '))
+if product_price >4000:
+    if product_price>=7000:
+        print('you will receive additonal gift')
+    print("congratualtions,you have received 20 % discount, current price{}".format(product_price*0.8))
+elif product_price >2000 and product_price <=3999:
+    print('congratuations, you have received 10% discount'.format(product_price*0.7))
+else:
+    print("sorry you are not eligible for any discount")
+        
+
+
+# In[ ]:
+
+
+number_activity=int(input('enter the number of activities of your project:'))
+A =int(input('enter the duration of activity A:'))
+B =int(input('enter the duration of activity B:'))
+C =int(input('enter the duration of activity C:'))
+D =int(input('enter the duration of activity D:'))
+Project_Duration=int(input('enter the total Project Duration:'))
+if A+B+C+D >60 and Project_Duration > 65:
+    print("this will consider the critical path of your project")
+else:
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
